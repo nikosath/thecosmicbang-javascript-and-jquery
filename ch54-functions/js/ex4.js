28f70778afe601ba5dfc78cc3cd9e782d463804f
Nikos Athanasakis <emidekol@gmail.com>
2017-03-21 16:32:09 +0200

    feat: Completed ex4 of ch54

--- ch54-functions/js/ex4.js
+++ ch54-functions/js/ex4.js
@@ -1,27 +1,7 @@
 function ex4() {
   'use strict';
 
-  // function Employee(name, salary, numberOfYears) {
-  //   var e = {};
-  //   e.name = name;
-  //   e.salary = salary;
-  //   e.numberOfYears = numberOfYears;
-  //
-  //   e.getBonus = function () {
-  //     switch (true) {
-  //     case (e.numberOfYears < 0.5):
-  //       return 0;
-  //     case (e.numberOfYears <= 5):
-  //       return e.salary / 2;
-  //     case (e.numberOfYears <= 10):
-  //       return e.salary;
-  //     case (e.numberOfYears > 10):
-  //       return 2 * e.salary;
-  //     }
-  //   };
-  //
-  //   return e;
-  // }
+  // Prototypal inheritance
   var employee = {
     name: '',
     salary: 0,
@@ -40,36 +20,43 @@ function ex4() {
     }
   };
 
-  function Employee(name, salary, numberOfYears) {
-    this.name = name;
-    this.salary = salary;
-    this.numberOfYears = numberOfYears;
-
-    this.getBonus = function () {
-      switch (true) {
-      case (this.numberOfYears < 0.5):
-        return 0;
-      case (this.numberOfYears <= 5):
-        return this.salary / 2;
-      case (this.numberOfYears <= 10):
-        return this.salary;
-      case (this.numberOfYears > 10):
-        return 2 * this.salary;
-      }
-    };
-  }
-
   var emp1 = Object.create(employee);
   emp1.name = 'James';
   emp1.salary = 1000;
   emp1.numberOfYears = 0.5;
 
-  var emp2 = new Employee('Katerina', 1000, 5);
+  var emp2 = Object.create(employee);
+  emp2.name = 'Katerina';
+  emp2.salary = 1000;
+  emp2.numberOfYears = 5;
+
+  // ES5 constructor function version of inheritance
+  function Employee(name, salary, numberOfYears) {
+    this.name = name | '';
+    this.salary = salary | 0;
+    this.numberOfYears = numberOfYears | 0;
+  }
+
+  Employee.prototype.getBonus = function () {
+    switch (true) {
+    case (this.numberOfYears < 0.5):
+      return 0;
+    case (this.numberOfYears <= 5):
+      return this.salary / 2;
+    case (this.numberOfYears <= 10):
+      return this.salary;
+    case (this.numberOfYears > 10):
+      return 2 * this.salary;
+    }
+  };
+
   var emp3 = new Employee('Kostas', 1000, 10);
+  var emp4 = new Employee();
 
   console.log(emp1);
   console.log(emp2);
   console.log(emp3);
+  console.log(emp4);
 
   // Output for index.html
   var ex4 = {};
@@ -77,7 +64,8 @@ function ex4() {
     return '<p>Ex4' +
       '<br>(James, 1000, 0.5) bonus: ' + emp1.getBonus() +
       '<br>(Katerina, 1000, 5) bonus: ' + emp2.getBonus() +
-      '<br>(Kostas, 1000, 0.5) bonus: ' + emp3.getBonus();
+      '<br>(Kostas, 1000, 10) bonus: ' + emp3.getBonus() +
+      '<br>() bonus: ' + emp4.getBonus();
   };
 
   return ex4;
