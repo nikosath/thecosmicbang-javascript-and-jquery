d7c385c1d42798f526410fcc58705eb34885ca68
Nikos Athanasakis <emidekol@gmail.com>
2017-03-25 23:26:19 +0200

    feat: Remove tabs without using indices

--- ch55-javascript-in-web-browsers/js/FuncChain.js
+++ ch55-javascript-in-web-browsers/js/FuncChain.js
@@ -1,4 +1,5 @@
 function FuncChain() {
+  'use strict';
   // queue of functions waiting to be scheduled
   this.queue = [];
   // currently scheduled function and its arguments
@@ -14,23 +15,33 @@ FuncChain.prototype = {
   // that: what 'this' should be inside fn
   // arg: a single argument for fn
   schedule: function (fn, delay, that, arg) {
+    // setTimeout executes the function with 'this' pointing to the global object, so we need to save it
     var self = this;
     setTimeout(function () {
+      // what was scheduled, we are now executing
       self.currentlyScheduled = null;
+      // check if we were given a 'this' (that) for fn
       if (that) {
         fn.call(that, arg);
       } else {
-        fn();
+        fn(arg);
       }
+      // if there are functions in the queue and there's no function scheduled, then schedule the first/oldest one
       if (self.queue.length && self.currentlyScheduled === null) {
         var item = self.queue.shift();
         self.schedule(item.fn, item.delay, item.that, item.arg);
       }
     }, delay);
+    // keep track of what we just scheduled with setTimeout
     self.currentlyScheduled = arguments;
   },
 
+  // fn: function to be delayed/scheduled
+  // delay: passed to setTimeout
+  // that: what 'this' should be inside fn
+  // arg: a single argument for fn
   addFunc: function (fn, delay, that, arg) {
+    // if there's another function scheduled, put fn in the queue
     if (this.currentlyScheduled) {
       this.queue.push({
         fn: fn,
--- ch55-javascript-in-web-browsers/js/ex1.js
+++ ch55-javascript-in-web-browsers/js/ex1.js
@@ -2,14 +2,8 @@ function ex1() {
   'use strict';
 
   // var URLS = ['https://www.google.com', 'http://www.nationalgeographic.com', 'http://cnn.com', 'https://www.zsl.org', 'http://www.telegraph.co.uk/', 'http://www.go2africa.com/', 'http://www.animalplanet.com/', 'http://www.theguardian.com/', 'http://travel.usnews.com/', 'http://www.independent.co.uk/'];
-  // var URLS = ['http://www.nationalgeographic.com', 'http://cnn.com', 'https://www.zsl.org'];
-
-  // var URLS = ['http://1/','http://2/'];
-  // var URLS = ['http://1/'];
   var URLS = ['http://1/', 'http://2/', 'http://3/', 'http://4/', 'http://5/', 'http://6/'];
-  // var URLS = ['http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/'];
-  // var URLS = ['http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/','http://127.0.0.1:8080/'];
-  // var urls = ['http://127.0.0.1:8080/', 'http://127.0.0.1:8080/', 'http://127.0.0.1:8080/'];
+  // var URLS = ['http://1/', 'http://2/'];
 
   var tabs = {
     tabGroup: [],
@@ -17,39 +11,17 @@ function ex1() {
     add: function (url) {
       console.log(time() + ' add tab ' + url);
       this.tabGroup.push(window.open(url));
-      // console.log(time() + ' tab name ' + this.tabGroup[0].name);
-
     },
     removeByIdx: function (index) {
-      var tab = this.tabGroup.splice(index, 1)[0].close();
+      this.tabGroup.splice(index, 1)[0].close();
       console.log(time() + ' remove tab with idx:' + index);
-      // tab.close();
+    },
+    removeTab: function (tab) {
+      // console.log(time() + ' remove tab:' + tab);
+      tab.close();
     },
     length: function () {
       return this.tabGroup.length;
-    },
-    addMany: function (urls, nextFunction) {
-      // console.log('nextFunction ' + nextFunction);
-      var tabs = this;
-      tabs.add(urls[0]);
-      urls = urls.slice(1);
-      console.log(urls.length);
-      if (urls.length > 0) {
-        setTimeout(function () {
-          console.log(urls.length);
-          if (urls.length === 1) {
-            // Add the last url
-            tabs.add(urls[0]);
-            // If nextFunction was provided, call it
-            // console.log('nextFunction ' + nextFunction);
-            if (nextFunction) {
-              nextFunction();
-            }
-          } else {
-            tabs.addMany(urls, nextFunction);
-          }
-        }, 1000);
-      }
     },
     tweakIndices: function (tabIndices) {
       console.log('Indices before tweaking: ' + tabIndices);
@@ -63,48 +35,12 @@ function ex1() {
       console.log('Indices after tweaking: ' + tabIndices);
       return tabIndices;
     },
-    removeTweakedIndices: function (tabIndices) {
-      var tabs = this;
-      tabs.removeByIdx(tabIndices[0]);
-      tabIndices = tabIndices.slice(1);
-
-      if (tabIndices.length > 0) {
-        setTimeout(function () {
-          if (tabIndices.length === 1) {
-            // Remove the last url
-            tabs.removeByIdx(tabIndices[0]);
-
-          } else {
-            tabs.removeTweakedIndices(tabIndices);
-          }
-        }, 1000);
-      }
-    },
-    removeMany: function (tabIndices) {
-      this.removeTweakedIndices(this.tweakIndices(tabIndices));
-    }
 
   };
 
   function time() {
     var time = new Date();
     return (time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-  }
-
-  function removeEvenOdd() {
-    var tabIndicesToRemove = [];
-    for (var base = 1; base >= 0; base -= 1) {
-      for (var idx = base; idx < tabs.length(); idx += 2) {
-        tabIndicesToRemove.push(idx);
-      }
-    }
-    // tabIndicesToRemove = [3,2,7,1,4,6,5,0];
-    console.log('Indices before tweaking: ' + tabIndicesToRemove);
-
-    setTimeout(function () {
-      tabs.removeMany(tabIndicesToRemove);
-    }, 1000);
-
   }
 
   function getEvenOddIndices(tabs) {
@@ -116,43 +52,16 @@ function ex1() {
     }
     return tabIndicesToRemove;
   }
+  function getEvenOddTabs(tabs) {
+    var evenOddTabs = [];
+    for (var base = 1; base >= 0; base -= 1) {
+      for (var idx = base; idx < tabs.length(); idx += 2) {
+        evenOddTabs.push(tabs.tabGroup[idx]);
+      }
+    }
+    return evenOddTabs;
+  }
 
-  // function startGame() {
-  //   tabs.addMany(URLS, removeEvenOdd);
-  // }
-  // do {
-  //   console.log(time() + ' before addMany');
-  //   setTimeout(startGame, 1000);
-  // } while (window.confirm('One more time?'));
-
-  // -----------------
-  // function main() {
-  //   var chain = funcChain(function () {
-  //     chain = chain.addFunc(tabs.add, 0, tabs, URLS[0]);
-  //     for (var i = 1; i < URLS.length; i++) {
-  //       chain = chain.addFunc(tabs.add, 2000, tabs, URLS[i]);
-  //     }
-  //
-  //     chain = chain.addFunc(function () {
-  //       // tabIndicesToRemove = tabs.tweakIndices([3,2,7,1,4,6,5,0]);
-  //       // var tabIndicesToRemove = tabs.tweakIndices([0,1,2]);
-  //       var tabIndicesToRemove = tabs.tweakIndices(getEvenOddIndices(tabs));
-  //       // chain = chain.addFunc(tabs.removeMany, 5000, tabIndicesToRemove);
-  //       chain = chain.addFunc(tabs.removeByIdx, 0, tabs, tabIndicesToRemove[0]);
-  //       for (var j = 1; j < tabIndicesToRemove.length; j++) {
-  //         chain = chain.addFunc(tabs.removeByIdx, 2000, tabs, tabIndicesToRemove[j]);
-  //       }
-  //
-  //       chain = chain.addFunc(function () {
-  //         if (window.confirm('One more time?')) {
-  //           main();
-  //         }
-  //       }, 0);
-  //
-  //     }, 5000);
-  //
-  //   }, 5000);
-  // }
   function main() {
     var chain = new FuncChain();
     chain.addFunc(function () {
@@ -162,17 +71,13 @@ function ex1() {
       }
 
       chain.addFunc(function () {
-        var tabIndicesToRemove = tabs.tweakIndices(getEvenOddIndices(tabs));
-        // chain.addFunc(tabs.removeMany, 5000, tabIndicesToRemove);
-        chain.addFunc(tabs.removeByIdx, 0, tabs, tabIndicesToRemove[0]);
-        for (var j = 1; j < tabIndicesToRemove.length; j++) {
-          chain.addFunc(tabs.removeByIdx, 2000, tabs, tabIndicesToRemove[j]);
+        var tabToRemove = getEvenOddTabs(tabs);
+        chain.addFunc(tabs.removeTab, 0, tabs, tabToRemove[0]);
+        for (var j = 1; j < tabToRemove.length; j++) {
+          chain.addFunc(tabs.removeTab, 2000, tabs, tabToRemove[j]);
         }
 
         chain.addFunc(function () {
-          // var response = alert('One more time?');
-          // alert('One more');
-          // console.log('heloo');
           if (window.confirm('Go one more time?')) {
             main();
           }
@@ -182,6 +87,31 @@ function ex1() {
 
     }, 5000);
   }
+  // function main() {
+  //   var chain = new FuncChain();
+  //   chain.addFunc(function () {
+  //     chain.addFunc(tabs.add, 0, tabs, URLS[0]);
+  //     for (var i = 1; i < URLS.length; i++) {
+  //       chain.addFunc(tabs.add, 2000, tabs, URLS[i]);
+  //     }
+  //
+  //     chain.addFunc(function () {
+  //       var tabIndicesToRemove = tabs.tweakIndices(getEvenOddIndices(tabs));
+  //       chain.addFunc(tabs.removeByIdx, 0, tabs, tabIndicesToRemove[0]);
+  //       for (var j = 1; j < tabIndicesToRemove.length; j++) {
+  //         chain.addFunc(tabs.removeByIdx, 2000, tabs, tabIndicesToRemove[j]);
+  //       }
+  //
+  //       chain.addFunc(function () {
+  //         if (window.confirm('Go one more time?')) {
+  //           main();
+  //         }
+  //       }, 0);
+  //
+  //     }, 5000);
+  //
+  //   }, 5000);
+  // }
 
   main();
 
